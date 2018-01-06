a2c7b7108497057f0fc96359a237853567faf583
Alexandre <bocketo@gmail.com>
2018-01-06 20:56:49 +0100

    Migration login google->firebase / bouton pour lecture dans la db

--- app/src/main/java/com/alexandre/kohakuapp/SignInActivity.java
+++ app/src/main/java/com/alexandre/kohakuapp/SignInActivity.java
@@ -17,6 +17,18 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -29,6 +41,12 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private final static String TAG = "SignIn Logs";
     private static int RC_SIGN_IN = 100;
 
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mDatabase;
+
+    private static final String t = "870480826350-8i26q4c3nauut70jimf9riu34ise65rf.apps.googleusercontent.com";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +54,14 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_signin);
         setTitle("Home");
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(t)
                 .requestEmail()
                 .build();
         // Build a GoogleSignInClient with the options specified by gso.
@@ -58,28 +81,66 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         gotoHomeSearch = (Button) findViewById(R.id.goto_home_search);
         gotoHomeSearch.setOnClickListener(this);
 
-
+        Button testButton = (Button) findViewById(R.id.testButton);
+        testButton.setOnClickListener(this);
 
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
+
     private void signIn() {
-        Log.e(TAG, "Trying to log in");
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    private void signOut() {
+    private void signOut(){
         Log.e(TAG, "Trying to log out");
+
+        // Log out the Firebase client
+        FirebaseAuth.getInstance().signOut();
+
+        // Log out the Google client
         mGoogleSignInClient.signOut()
                 .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         Log.e(TAG, "Log Out Successful");
                         updateUI(null);
+                    }
+                });
+
+        updateUI(null);
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
                     }
                 });
     }
@@ -98,13 +159,16 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+   private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            Log.e(TAG, "Logged in successful");
-            updateUI(account);
+            Log.e(TAG, "Login successful");
+
+            firebaseAuthWithGoogle(account);
+
+            //updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -120,17 +184,18 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         startActivity(myIntent);
     }
 
-    private void updateUI(GoogleSignInAccount account){
-        if(account != null){
+    private void updateUI(FirebaseUser u){
+        if(u != null){
             signInButton.setVisibility(View.GONE);
             signOutButton.setVisibility(View.VISIBLE);
-            signInStatus.setText("Logged in as " + account.getEmail());
+            signInStatus.setText("Logged in as " + u.getEmail());
         }else{
             signInButton.setVisibility(View.VISIBLE);
             signOutButton.setVisibility(View.GONE);
             signInStatus.setText("Not Logged in");
         }
     }
+
 
     @Override
     public void onClick(View v){
@@ -144,7 +209,51 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.goto_home_search:
                 gotoHomeSearch();
                 break;
+            case R.id.testButton:
+                testDb();
+                break;
         }
+    }
+
+
+    private void testDb(){
+        /* Tests pour la database */
+        Log.e(TAG, mDatabase.child("test").getKey());
+        DatabaseReference ref = mDatabase.child("mess/");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String b = "";
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    // TODO: handle the post
+                    String s, e;
+                    s = postSnapshot.getKey();
+                    e = postSnapshot.getValue(String.class);
+
+                    b = b + s + ": " + e + ", ";
+                }
+
+                showDbText(b);
+
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                /*String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    private void showDbText(String s1){
+        TextView t1 = (TextView) findViewById(R.id.dbText1);
+        t1.setText(s1);
     }
 
 }
